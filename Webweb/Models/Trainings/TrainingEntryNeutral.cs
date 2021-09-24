f676b403ba2e963ec9118e55c480bfdc7a86137a
L00king4 <penetratemyass25@gmail.com>
2021-09-24 23:59:43 +0200

    finished GetTrainingByMonth

---
+++ Webweb/Models/Trainings/TrainingEntryNeutral.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webweb.Models.Trainings
+{
+    public class TrainingEntryNeutral : TrainingEntry
+    {
+        public int TraineeID { set; get; }
+    }
+}
--- Webweb/Services/Repos/Trainings/TrainingRepo.cs
+++ Webweb/Services/Repos/Trainings/TrainingRepo.cs
@@ -22,6 +22,7 @@ namespace Webweb.Services.Repos.Trainings
 
         public async Task<TrainingMonth> GetTrainingMonthAsync(DateTime date, AgeGroup ageGroup)
         {
+            var start = DateTime.Now;
             var trainingMonth = new TrainingMonth();
             var trainings = _db.Trainings.AsEnumerable().Where(x => (ageGroup == AgeGroup.All || x.AgeGroup == ageGroup) && x.Date.Value.Month == date.Month && x.Date.Value.Year == date.Year).ToList();
             var trainees = _db.Trainees.AsEnumerable().Where(x => ageGroup == AgeGroup.All || x.AgeGroup == ageGroup).ToList();
@@ -29,36 +30,64 @@ namespace Webweb.Services.Repos.Trainings
             var singlePayments = _db.TrainingPayments.AsEnumerable().Where(x => trainings.Any(y => y.ID == x.EventID)).ToList();
             var spanPayments = _db.TrainingSpanPayments.AsEnumerable().Where(x => trainings.Any(y => y.ID == x.EventID)).ToList();
 
-            var singlePaymentsGroup = singlePayments.GroupBy(x => (x.EventID, x.TraineeID), x => new TrainingEntry { PayedAmount = x.Amount, EventID = x.EventID });
-            var attendancesGroup = attendances.GroupBy(x => (x.EventID, x.TraineeID), x => new TrainingEntry { HasAttended = true, EventID = x.EventID });
+            //var singlePaymentsGroup = singlePayments.GroupBy(x => (x.EventID, x.TraineeID), x => new TrainingEntry { PayedAmount = x.Amount, EventID = x.EventID });
+            //var attendancesGroup = attendances.GroupBy(x => (x.EventID, x.TraineeID), x => new TrainingEntry { HasAttended = true, EventID = x.EventID });
 
-            foreach (var trainee in trainees)
-            {
-                trainingMonth.TrainingTrainees = trainingMonth.TrainingTrainees.Append(new TrainingTrainee() { Trainee = trainee });
-            }
+            //foreach (var trainee in trainees)
+            //{
+            //    trainingMonth.TrainingTrainees = trainingMonth.TrainingTrainees.Append(new TrainingTrainee() { Trainee = trainee });
+            //}
 
-            var groups = singlePaymentsGroup.Concat(attendancesGroup).GroupBy(x => x.Key);
-            foreach (var trainee in trainingMonth.TrainingTrainees)
-            {
-                foreach (var training in trainings)
-                {
-                    try
-                    {
-                        var newTrainingEntry = groups.First(x => x.Key.EventID == training.ID && x.Key.TraineeID == trainee.Trainee.ID)
-                            .Select(x => new TrainingEntry
+            //var groups = singlePaymentsGroup.Concat(attendancesGroup).GroupBy(x => x.Key);
+            //foreach (var trainee in trainingMonth.TrainingTrainees)
+            //{
+            //    foreach (var training in trainings)
+            //    {
+            //        try
+            //        {
+            //            var newTrainingEntry = groups.First(x => x.Key.EventID == training.ID && x.Key.TraineeID == trainee.Trainee.ID)
+            //                .Select(x => new TrainingEntry
+            //                {
+            //                    EventID = x.Key.Item1,
+            //                    PayedAmount = x.Sum(y => y.PayedAmount),
+            //                    HasAttended = x.Any(y => y.HasAttended)
+            //                }).First();
+            //            trainee.TrainingEntries = trainee.TrainingEntries.Append(newTrainingEntry);
+            //        }
+            //        catch
+            //        {
+
+            //        }
+            //    }
+            //}
+
+            var neutralEntriesFromPayments = singlePayments.GroupBy(d => (d.EventID, d.TraineeID))
+                        .Select(
+                            g => new TrainingEntryNeutral
                             {
-                                EventID = x.Key.Item1,
-                                PayedAmount = x.Sum(y => y.PayedAmount),
-                                HasAttended = x.Any(y => y.HasAttended)
-                            }).First();
-                        trainee.TrainingEntries = trainee.TrainingEntries.Append(newTrainingEntry);
-                    }
-                    catch
-                    {
-
-                    }
-                }
-            }
+                                EventID = g.Key.EventID,
+                                TraineeID = g.Key.TraineeID,
+                                PayedAmount = g.Sum(s => s.Amount),
+                                HasAttended = false
+                            });
+            var neutralEntriesFromAttendances = attendances.GroupBy(d => (d.EventID, d.TraineeID))
+                           .Select(g => new TrainingEntryNeutral
+                           {
+                               EventID = g.Key.EventID,
+                               TraineeID = g.Key.TraineeID,
+                               PayedAmount = 0,
+                               HasAttended = true
+                           });
+            var allNeutralEntries = neutralEntriesFromPayments.Concat(neutralEntriesFromAttendances);
+            var trainingTraineesGroups = allNeutralEntries.GroupBy(x => (x.TraineeID, x.EventID))
+                        .Select(
+                x => new TrainingEntryNeutral
+                {
+                    EventID = x.Key.EventID,
+                    TraineeID = x.Key.TraineeID,
+                    PayedAmount = x.Sum(y => y.PayedAmount),
+                    HasAttended = x.Any(y => y.HasAttended)
+                }).GroupBy(x => x.TraineeID, x => (TrainingEntry)x);
 
             foreach (var spanPayment in _mapper.Map<IEnumerable<TrainingPayedSpan>>(spanPayments))
             {
@@ -72,8 +101,28 @@ namespace Webweb.Services.Repos.Trainings
                 }
             }
 
+
+            IEnumerable<TrainingEntry> emptyList = new List<TrainingEntry>();
+            foreach (var trainee in trainees)
+            {
+                IEnumerable<TrainingEntry> trainingEntries = trainingTraineesGroups.FirstOrDefault(x => x.Key == trainee.ID);
+                if (trainingEntries == null)
+                {
+                    trainingEntries = emptyList;
+                }
+                trainingMonth.TrainingTrainees = trainingMonth.TrainingTrainees.Append(
+                new TrainingTrainee
+                {
+                    Trainee = trainee,
+                    TrainingEntries = trainingEntries,//trainingTraineesGroups.First(x => x.Key == trainee.ID),
+                    TrainingPayedSpans = _mapper.Map<IEnumerable<TrainingPayedSpan>>(spanPayments.Where(x => x.TraineeID == trainee.ID).ToList())
+                }
+                );
+
+            }
             trainingMonth.TrainingInfos = _mapper.Map<IEnumerable<TrainingInfo>>(trainings);
 
+            Console.WriteLine((DateTime.Now - start).Milliseconds);
             return trainingMonth;
         }
     }
